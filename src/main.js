db127d41fdb77e89fe7fd9b224fc13c832960804
ACEMAXW <acemaxw@foxmail.com>
2021-07-13 22:32:48 +0800

    0.0.0.0

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import 'swiper/dist/css/swiper.min.css'
+import 'swiper/dist/js/swiper.min'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
+
+Vue.use(VueAxios, axios)=[';']
+
 new Vue({
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
+
